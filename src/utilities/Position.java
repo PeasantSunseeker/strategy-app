acfa33df8b499c75b928a02bcb2d142f72dad4e6
Aaron Brainard <aarobrai@gmail.com>
2017-01-22 20:19:37 -0500

    setup weather API and classes

---
+++ src/utilities/Position.java
@@ -0,0 +1,14 @@
+package utilities;
+
+/**
+ * PROJECT: seniordesign
+ * AUTHOR: aaron
+ * DATE: 1/22/17
+ * <p>
+ * DESCRIPTION:
+ */
+public class Position {
+    private float latitude;
+    private float longitude;
+    private float elevation; //can change this data type if needed, hasn't been established yet
+}
