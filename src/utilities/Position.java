48929bb2974d12fba3e54d54bb477b9ed8d3bf42
Adam Hubbell <adam32122@gmail.com>
2017-01-23 20:00:10 -0500

    added angle and direction to Position class

--- src/utilities/Position.java
+++ src/utilities/Position.java
@@ -15,7 +15,29 @@ public class Position {
 
     private float latitude;
     private float longitude;
-    private float elevation; //can change this data type if needed, hasn't been established yet
+    private float elevation;
+    private float direction; //degrees clockwise from due north
+    private float angle; //angle of the road 0 is flat, 40 is a steep uphill, -40 is a steep downhill
+    
+    //can change this data type if needed, hasn't been established yet
+
+
+    public float getDirection() {
+        return direction;
+    }
+
+    public void setDirection(float direction) {
+        this.direction = direction;
+    }
+
+    public float getAngle() {
+        return angle;
+    }
+
+    public void setAngle(float angle) {
+        this.angle = angle;
+    }
+
 
     public Position(float latitude, float longitude, float elevation) {
         this.latitude = latitude;
